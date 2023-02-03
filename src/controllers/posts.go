2d0eb6f39b7757ab6b3ab4dd167c35bd40e587e1
thiago tanaka <thiago.tanaka.dev@gmail.com>
2023-02-03 21:26:04 +0900

    add posts validation

--- src/controllers/posts.go
+++ src/controllers/posts.go
@@ -32,6 +32,11 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 
 	post.AuthorID = userId
 
+	if err = post.Prepare(); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
 	db, err := database.Connect()
 	if err != nil {
 		responses.Error(w, http.StatusInternalServerError, err)
--- src/models/post.go
+++ src/models/post.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
 
 type Post struct {
 	ID         uint64    `json:"id,omitempty"`
@@ -10,4 +15,36 @@ type Post struct {
 	AuthorNick string    `json:"authorNick,omitempty"`
 	Likes      uint64    `json:"likes"`
 	CreatedAt  time.Time `json:"createdAt,omitempty"`
+}
+
+func (post *Post) Prepare() error {
+	post.format()
+
+	if err := post.validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (post *Post) validate() error {
+	if post.Title == "" {
+		return errors.New("Title is required")
+	}
+	fmt.Println("*" + post.Content + "*")
+
+	if post.Content == "" {
+		return errors.New("Content is required")
+	}
+
+	if post.AuthorID == 0 {
+		return errors.New("Author is required")
+	}
+
+	return nil
+}
+
+func (post *Post) format() {
+	post.Title = strings.TrimSpace(post.Title)
+	post.Content = strings.TrimSpace(post.Content)
 }
