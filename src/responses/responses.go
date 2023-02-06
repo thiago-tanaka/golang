8f8cd91db3e83089f5866d5416b4246651340585
thiago tanaka <thiago.tanaka.dev@gmail.com>
2023-02-07 08:09:01 +0900

    change names

--- src/responses/responses.go
+++ src/responses/responses.go
@@ -11,17 +11,17 @@ func JSON(w http.ResponseWriter, status int, data interface{}) {
 	w.WriteHeader(status)
 
 	if data != nil {
-		if error := json.NewEncoder(w).Encode(data); error != nil {
-			log.Fatal(error)
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			log.Fatal(err)
 		}
 	}
 }
 
 func Error(w http.ResponseWriter, status int, error error) {
 	JSON(w, status, struct {
-		Error string `json:"error"`
+		Messages string `json:"messages"`
 	}{
-		Error: error.Error(),
+		Messages: error.Error(),
 	})
 
 }
